8e26c297d276eabddf14d04480eaa60f74eeac55
MartinNevlaha <martin.nevlaha@gmail.com>
2020-12-04 16:58:30 +0100

    create basic structure of navigation

---
+++ navigation/AppNavigation.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+const AppNavigation = props => {
+
+  return (
+    <NavigationContainer>
+
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigation;
---
+++ navigation/DegustatorNavigation.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Ionicons } from "@expo/vector-icons";
+
+const DegustatorStackNavigator = createStackNavigator();
+
+export const DegustatorNavigator = () => {
+  return (
+    <DegustatorStackNavigator.Navigator>
+      
+    </DegustatorStackNavigator.Navigator>
+  )
+};
