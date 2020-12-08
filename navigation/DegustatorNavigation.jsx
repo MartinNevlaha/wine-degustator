4660d697ecdb79ff369deee3216e8724121dcc61
MartinNevlaha <martin.nevlaha@gmail.com>
2020-12-08 20:56:33 +0100

    add linear gradient to header

--- navigation/DegustatorNavigation.jsx
+++ navigation/DegustatorNavigation.jsx
@@ -1,14 +1,35 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Ionicons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+
+import Colors from "../constants/Colors";
 
 import DegustatorScreen from "../screens/DegustatorScreen";
+
+const defaultNavOpt = {
+  headerBackground: () => (
+    <LinearGradient
+      colors={["#611C2A", "white"]}
+      style={{ flex: 1 }}
+      start={{ x: 0.5, y: 0 }}
+      end={{ x: 1.2, y: 0 }}
+    />
+  ),
+  headerTitleStyle: {
+    fontFamily: "open-sans-bold",
+  },
+  headerBackTitleStyle: {
+    fontFamily: "open-sans",
+  },
+  headerTintColor: "white",
+};
 
 const DegustatorStackNavigator = createStackNavigator();
 
 export const DegustatorNavigation = () => {
   return (
-    <DegustatorStackNavigator.Navigator>
+    <DegustatorStackNavigator.Navigator screenOptions={defaultNavOpt}>
       <DegustatorStackNavigator.Screen
         name="Degustácia"
         component={DegustatorScreen}
