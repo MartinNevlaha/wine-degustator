ed270b7db472b028ac06e9209e4fd307eaad09ce
MartinNevlaha <martin.nevlaha@gmail.com>
2021-01-08 15:16:15 +0100

    fix wineInfo container and activityindicator

--- components/Degustator/DegTable.jsx
+++ components/Degustator/DegTable.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import { FontAwesome5 } from "@expo/vector-icons";
 import { connect } from "react-redux";
 import {
   widthPercentageToDP as wp,
--- components/Degustator/Wineinfo.jsx
+++ components/Degustator/Wineinfo.jsx
@@ -1,52 +1,61 @@
 import React from "react";
 import { View, ActivityIndicator, StyleSheet } from "react-native";
 import { connect } from "react-redux";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 import OwnText from "../UI/Text";
 
 const WineInfo = (props) => {
   return (
     <View style={styles.container}>
-      {!props.loading ?
-      <React.Fragment>
-      <OwnText>
-        Súťažná kat.:
-        {props.wineInfo.competitiveCategory
-          ? props.wineInfo.competitiveCategory
-          : "Zadaj číslo vína"}
-      </OwnText>
-      <OwnText>
-        Farba:
-        {props.wineInfo.color ? props.wineInfo.color : "Zadaj číslo vína"}
-      </OwnText>
-      <OwnText>
-        Charakter:
-        {props.wineInfo.character
-          ? props.wineInfo.character
-          : "Zadaj číslo vína"}
-      </OwnText>
-      <OwnText>
-        Ročník:
-        {props.wineInfo.vintage ? props.wineInfo.vintage : "Zadaj číslo vína"}
-      </OwnText>
-      </React.Fragment> :
-      <ActivityIndicator  size="small" color="white" />}
+      {!(props.loading && !props.sending) ? (
+        <React.Fragment>
+          <OwnText>
+            Súťažná kat.:
+            {props.wineInfo.competitiveCategory
+              ? " " + props.wineInfo.competitiveCategory
+              : " Zadaj číslo vína"}
+          </OwnText>
+          <OwnText>
+            Farba:
+            {props.wineInfo.color
+              ? " " + props.wineInfo.color
+              : " Zadaj číslo vína"}
+          </OwnText>
+          <OwnText>
+            Charakter:
+            {props.wineInfo.character
+              ? " " + props.wineInfo.character
+              : " Zadaj číslo vína"}
+          </OwnText>
+          <OwnText>
+            Ročník:
+            {props.wineInfo.vintage
+              ? " " + props.wineInfo.vintage
+              : " Zadaj číslo vína"}
+          </OwnText>
+        </React.Fragment>
+      ) : (
+        <ActivityIndicator size="small" color="white" />
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    minWidth: "18%",
-    minHeight: "16%"
-  }
+    width: wp("20%"),
+    height: hp("15%")
+  },
 });
 
 const mapStateToProps = (state) => ({
   wineInfo: state.wineInfo.wineInfo,
-  loading: state.wineInfo.loading
+  loading: state.wineInfo.loading,
+  sending: state.wineInfo.sending,
 });
-
-
 
 export default connect(mapStateToProps, null)(WineInfo);
